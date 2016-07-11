c591288552e2519933633da0b0215592419862c9
Bhuvana Chandra Inampudi <bhuvanachandrainampudi@seas1038.wireless-pennnet.upenn.edu>
2016-07-11 15:00:45 -0400

    Added skeleton to support Observers

---
+++ Assets/scripts/Observer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Observer : MonoBehaviour {
+
+	string observerTrace = "";
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (MessageBus.HasUnreadMsg ()) {
+
+
+		}
+	}
+
+
+	/* Returns the names of all SmartObjects in the Field Of View 
+	 * of the observer.
+	 * SIDE NOTES : All the Smart Objects in the scene will have a
+	 * 'SmartObject' tag attaced to them, and variable of SmartObject
+	 * type has a 'name' field. This function should return the names
+	 * of all the objects with tag 'SmartObject' in its field of view.
+	*/
+	List<string> GetObjectsInFieldOfView () {
+
+	}
+}
---
+++ Assets/scripts/Utils/MessageBus.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+public static class MessageBus {
+
+	static string trace = "";
+	static string traceMsg = "";
+	static bool unread = false;
+	static string actor1 = "";
+	static string actor2 = "";
+
+	public static void PublishMessage(string msg, string actorName1, string actorName2) {
+
+		actor1 = actorName1;
+		actor2 = actorName2;
+		traceMsg = msg;
+		trace = trace + traceMsg;
+		unread = true;
+	}
+
+	public static bool MsgContainsActor(string actorName) {
+
+		if (actorName.Equals (actor1) || actorName.Equals (actor2))
+			return true;
+		else
+			return false;
+		
+	}
+
+	public static bool HasUnreadMsg() {
+
+		return unread;
+	}
+
+	public static void ResetMsgBus() {
+		
+		actor1 = "";
+		actor2 = "";
+		traceMsg = "";
+		unread = false;
+	}
+}
